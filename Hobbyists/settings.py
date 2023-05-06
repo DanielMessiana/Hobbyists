a98fd47995655a816100cc6095e7dbe0861c4589
Dan <danielmessiana@gmail.com>
2023-05-06 16:41:25 -0400

    yaml

--- Hobbyists/settings.py
+++ Hobbyists/settings.py
@@ -33,7 +33,7 @@ SECURE_HSTS_PRELOAD = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.0.157", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.0.157", "127.0.0.1", 'localhost']
 
 
 # Application definition
