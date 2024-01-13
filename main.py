d6adb693b9406f910a0c504c8c0a5abb2e1430aa
Daniel Messiana <40705754+DanielMessiana@users.noreply.github.com>
2024-01-13 12:20:46 -0500

    django to flask

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+from flask import url_for
+
+hobbyists = Flask(__name__)
+
+@hobbyists.route("/")
+def start():
+	return render_template("start.html")
+
+#@hobbyists.route("/survey")
